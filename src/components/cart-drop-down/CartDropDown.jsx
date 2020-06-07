c0e55686db4fec03ef72afaa2b84699107163bd3
rutvik11062000 <rutvik.chandla@gmail.com>
2020-06-07 19:40:56 +0530

    showing cartItems in drowpdown menu

--- src/components/cart-drop-down/CartDropDown.jsx
+++ src/components/cart-drop-down/CartDropDown.jsx
@@ -1,16 +1,24 @@
-import './CartDropDownStyles.scss'
-import CustomButton from '../custombutton/CustomButton'
-import React from 'react'
+import "./CartDropDownStyles.scss";
+import CustomButton from "../custombutton/CustomButton";
+import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../cart-items/cartItem.component";
 
-const CartDropDown = () => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items">
+const CartDropDown = ({ cartItems }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>Go Check Out</CustomButton>
+    </div>
+  );
+};
 
-            </div>
-            <CustomButton>Go Check Out</CustomButton>
-        </div>
-    )
-}
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
 
-export default CartDropDown;
+export default connect(mapStateToProps)(CartDropDown);
