4adbf18b3173ba846a285025727cf42b8ed5b72f
rutvik11062000 <rutvik.chandla@gmail.com>
2020-06-22 22:11:18 +0530

    removing logger from development version

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 export default { store, persistor };
